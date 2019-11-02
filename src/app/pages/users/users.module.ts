1fc87a68ae4d93683efb70dcc7152569a795dcc7
Jaohar Oseni <mekenjao@gmail.com>
2019-11-02 09:46:12 +0000

    Fixed routing Error in Create User component

--- src/app/pages/users/users.module.ts
+++ src/app/pages/users/users.module.ts
@@ -3,14 +3,16 @@ import { RouterModule } from '@angular/router'
 import { SharedModule } from '../shared.module'
 
 import { UsersComponent } from './users.component'
+import { CreateUserComponent } from '../create-user/create-user.component'
 
 export const routes = [
-  { path: '', component: UsersComponent, pathMatch: 'full' }
+  { path: '', component: UsersComponent, pathMatch: 'full' },
+  { path: 'create', component: CreateUserComponent}
 ]
 
 @NgModule({
   imports: [RouterModule.forChild(routes), SharedModule],
-  declarations: [UsersComponent],
+  declarations: [UsersComponent, CreateUserComponent],
   providers: []
 })
 export class UsersModule {}
