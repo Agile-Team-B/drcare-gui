566a92005a3cf49d78f025530147e269c85fccb7
Jaohar Oseni <mekenjao@gmail.com>
2019-11-13 14:35:05 +0000

    - Implemented the search for GP - implemented the update for GP

--- src/app/pages/general-practitioner/general-practitioner.component.ts
+++ src/app/pages/general-practitioner/general-practitioner.component.ts
@@ -1,23 +1,144 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, TemplateRef,
+  ViewChild,
+  ViewEncapsulation } from '@angular/core';
+import { NgbModal, NgbModalRef } from '@ng-bootstrap/ng-bootstrap';
 import { GeneralPractitionerService } from './general-practitioner.service';
+import { LoaderService } from '../../services/loader.service';
+import { FormGroup, Validators, FormBuilder } from '@angular/forms';
+
+import { GridModel } from '../shared/grid/grid.model';
+import { trimSpacesValidate, emailValidator } from '../../validators';
 
 @Component({
   selector: 'app-general-practitioner',
   templateUrl: './general-practitioner.component.html',
-  styleUrls: ['./general-practitioner.component.css']
+  styleUrls: ['./general-practitioner.component.css'],
+  encapsulation: ViewEncapsulation.None
 })
 export class GeneralPractitionerComponent implements OnInit {
+  public modalRef: NgbModalRef;
+  public searchForm: FormGroup;
+  public editGPForm: FormGroup;
 
-  constructor(public gpService: GeneralPractitionerService) { }
-  public gpUser;
+  @ViewChild('content', null)
+  private content;
+  @ViewChild('isAdminTmpl', null)
+  isAdminTmpl: TemplateRef<any>;
+  @ViewChild('actionTmpl', null)
+  actionTmpl: TemplateRef<any>;
 
-  ngOnInit() {
-    this.fetchAllGP();
+  selectedUserId: number;
+  settings: GridModel;
+  columns: Array<any> = [
+    {
+      name: 'Name',
+      prop: 'name'
+    },
+    {
+      name: 'Email',
+      prop: 'email'
+    },
+    {
+      name: 'Username',
+      prop: 'username'
+    },
+    {
+      name: 'Admin',
+      prop: 'isAdmin',
+      cellTemplate: null
+    },
+    {
+      name: 'Action',
+      prop: 'id',
+      cellTemplate: null
+    }
+  ];
+
+  constructor(
+    fb: FormBuilder,
+    private modalService: NgbModal,
+    private gpService: GeneralPractitionerService,
+    public loaderService: LoaderService
+  ) {
+    this.searchForm = fb.group({
+      usernameSearch: ['']
+    });
+    Object.keys(this.searchForm.controls).map(key => {
+      this[key] = this.searchForm.controls[key];
+    });
+    this.settings = new GridModel(this.columns, 10);
+    this.editGPForm = fb.group({
+      name: ['', [trimSpacesValidate]],
+      username: ['', [trimSpacesValidate]],
+      email: ['', Validators.compose([trimSpacesValidate, emailValidator])],
+      isAdmin: [false],
+      userType: ['GP']
+    });
+    Object.keys(this.editGPForm.controls).map(key => {
+      this[key] = this.editGPForm.controls[key];
+    });
   }
 
+  closeModal(): void {
+    this.modalService.dismissAll();
+    this.loadGP();
+  }
 
-  fetchAllGP() {
-      this.gpService.fetchAllGP().subscribe(res => this.gpUser = res);
+  edit(row): void {
+    this.selectedUserId = row.id;
+    this.modalService.open(this.content, { size: 'lg' });
+
+    for (const key in row) {
+      if (this.editGPForm.controls[key]) {
+        this.editGPForm.controls[key].setValue(row[key]);
+      }
+    }
+  }
+
+  ngOnInit() {
+    this.loadGP();
+  }
+
+  submitEditForm(values: any): void {
+    const body = { id: this.selectedUserId, ...values };
+    console.log('val: ', body);
+
+    this.gpService.updateGP(body).subscribe(data => {
+      console.log('resData', data);
+      this.closeModal();
+    });
+  }
+
+  checkGridDataLoad(): boolean {
+    return this.settings.rows.length > 0;
+  }
+
+  syncGridData(gridModel: GridModel): void {
+    this.loadGP(gridModel);
+  }
+
+  searchGP(values): void {
+    const body = { username: values.usernameSearch };
+    console.log('body', body);
+
+    this.gpService.searchGP(body).subscribe(data => {
+      const settings = { ...this.settings };
+
+      settings.rows = data;
+      this.settings = { ...settings };
+    });
+  }
+
+  loadGP(gridModel: GridModel = this.settings): void {
+    this.gpService.fetchAllGP().subscribe(data => {
+      const settings = { ...gridModel };
+      const colLen = settings.columns.length;
+
+      settings.rows = data;
+      settings.columns[colLen - 2].cellTemplate = this.isAdminTmpl;
+      settings.columns[colLen - 1].cellTemplate = this.actionTmpl;
+      this.settings = { ...settings };
+    });
   }
 
 }
--- src/app/pages/general-practitioner/general-practitioner.service.ts
+++ src/app/pages/general-practitioner/general-practitioner.service.ts
@@ -1,5 +1,11 @@
 import { Injectable } from '@angular/core';
 import { ApiCallService } from '../../services/api-call.service';
+import {
+  IGetGP,
+  ISearchGP,
+  IUpdateGP
+} from './general-practitioner.model'
+import { isNullOrUndefined } from 'util';
 
 @Injectable({
   providedIn: 'root'
@@ -8,8 +14,20 @@ export class GeneralPractitionerService {
 
   constructor(public apiCaller: ApiCallService) { }
 
-
-  fetchAllGP() {
+  fetchAllGP: IGetGP = () => {
       return this.apiCaller.get('general-practitioner/list');
   }
+
+  searchGP: ISearchGP = body => {
+    if (isNullOrUndefined(body.username) || body.username === '') {
+      return this.apiCaller.get('general-practitioner/list');
+    }
+
+    return this.apiCaller.get(`general-practitioner/search/${body.username}`);
+  }
+
+  updateGP: IUpdateGP = body => {
+    return this.apiCaller.put('general-practitioner/update', body);
+  }
+
 }
